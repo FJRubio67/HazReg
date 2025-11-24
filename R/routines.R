d003e52f1bbf740b6b90687a5eec0a8dc8bb8cc8
Francisco Javier Rubio <33929387+FJRubio67@users.noreply.github.com>
2025-11-24 23:06:13 +0000

    Update baseline hazard names in simGH function
    Changed baseline hazard distribution names in simGH and documentation from abbreviations (LN, LL, GG, G, W) to full names (LogNormal, LogLogistic, GenGamma, Gamma, Weibull) for clarity and consistency.

--- R/routines.R
+++ R/routines.R
@@ -1215,7 +1215,7 @@ Conf_Int <- function(FUN,MLE,level=0.95,index=NULL){
 #----------------------------------------------------------------------------------------
 #' simGH function: Function to simulate times to event from a model with a GH structure
 #' for different parametric baseline hazards.
-#' Distributions: LN, LL, GG, G (Gamma), W (Weibull), PGW, EW.
+#' Distributions: LogNormal, LogLogistic, GenGamma, Gamma, Weibull, PGW, EW.
 #' See: https://github.com/FJRubio67/HazReg
 #----------------------------------------------------------------------------------------
 #' @param seed  : seed for simulation
@@ -1240,13 +1240,13 @@ simGH <- function(seed, n, des = NULL, des_h = NULL, des_t = NULL, theta,
   if(!is.null(des_t)) des_t <- as.matrix(des_t)
 
   # Baseline hazard
-  if(baseline == "LN")      quantf <- function(p) qlnorm(p, theta[1], theta[2])
-  if(baseline == "LL")      quantf <- function(p) qllogis(p, theta[1], theta[2])
-  if(baseline == "G")       quantf <- function(p) qgamma(p, theta[1], theta[2])
-  if(baseline == "W")       quantf <- function(p) qweibull(p, theta[1], theta[2])
+  if(baseline == "LogNormal")      quantf <- function(p) qlnorm(p, theta[1], theta[2])
+  if(baseline == "LogLogistic")      quantf <- function(p) qllogis(p, theta[1], theta[2])
+  if(baseline == "Gamma")       quantf <- function(p) qgamma(p, theta[1], theta[2])
+  if(baseline == "Weibull")       quantf <- function(p) qweibull(p, theta[1], theta[2])
   if(baseline == "PGW")     quantf <- function(p) qpgw(p, theta[1], theta[2], theta[3])
   if(baseline == "EW")      quantf <- function(p) qew(p, theta[1], theta[2], theta[3])
-  if(baseline == "GG")      quantf <- function(p) qggamma(p, theta[1], theta[2], theta[3])
+  if(baseline == "GenGamma")      quantf <- function(p) qggamma(p, theta[1], theta[2], theta[3])
 
   # Uniform variates used in the simulation
   set.seed(seed)
