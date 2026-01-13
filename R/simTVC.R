cf02d7043fad55796b1fef32c0099cf238a62e8d
Francisco Javier Rubio <fxrubio@gmail.com>
2026-01-13 10:21:38 +0000

    Update simTVC.R

--- R/simTVC.R
+++ R/simTVC.R
@@ -9,10 +9,29 @@ library(SimLT)
 #------------------------------------------------------------------------
 # Baseline design matrix simulation
 #------------------------------------------------------------------------
+# Seed
+seed <- 123
+set.seed(seed)
+
 # Sample size
 n <- 1000
 # Design matrix
-Xcf <- simDesMatrix(seed = 123, n = n, admin.cens = "31-12-2015", scale.age = FALSE,
+Xcf <- simDesMatrix(seed = seed, n = n, admin.cens = "31-12-2015", scale.age = FALSE,
                     site = "colon", sex = "female")
 head(Xcf)
 
+# Number of individual contact points
+
+lambda0 <- 2
+ncp <- rpois(n = n, lambda = lambda0)
+ncps <- sum(ncp)
+
+# IDs
+id <- rep(seq_along(ncp), times = ncp)
+
+
+# Contact points
+# ncp: vector of counts, length n
+# fup: vector of follow-up times, length n
+
+id <- rep(seq_along(ncp), times = ncp)
